239dda8ffd90bdb5e7c6e6a19900cdaaf4e5925f
ld100 <al@railsware.com>
2009-04-16 19:38:55 +0300

    addres rescue_from_template_exceptions plugin: http://github.com/iSabanin/rescue_from_templates_exceptions/tree/master

---
+++ config/initializers/rescue_from_templates_exceptions.rb
@@ -0,0 +1,3 @@
+# Rescue from templates initialiser
+# See http://github.com/iSabanin/rescue_from_templates_exceptions/tree/master for details
+ActionController::Base.rescue_from_templates_exceptions = true
