8e8fa2c6e22ceb2b3b08b0cf104fdc2625b6d52c
Alexander Lockshyn <ld100@c1.frenetic.ws>
2009-05-08 00:21:47 +0300

    Added login and logout shortcuts to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 ActionController::Routing::Routes.draw do |map|
+  
+  map.login 'login', :controller => 'user_sessions', :action => 'new'  
+  map.logout 'logout', :controller => 'user_sessions', :action => 'destroy'  
   map.resource :user_session
   
   map.resource :account, :controller => "users"  
