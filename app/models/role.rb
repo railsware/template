f16c92b376af6efbe45f79b518380069fc7298b1
Alexander Lockshyn <ld100@c1.frenetic.ws>
2009-05-16 16:03:51 +0300

    Added fileds description to models via What Column

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,12 @@
 class Role < ActiveRecord::Base
+
+  # === List of columns ===
+  #   id                : integer 
+  #   name              : string 
+  #   authorizable_type : string 
+  #   authorizable_id   : integer 
+  #   created_at        : datetime 
+  #   updated_at        : datetime 
+  # =======================
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ActiveRecord::Base
+
+  # === List of columns ===
+  #   id                : integer 
+  #   login             : string 
+  #   email             : string 
+  #   crypted_password  : string 
+  #   password_salt     : string 
+  #   persistence_token : string 
+  #   created_at        : datetime 
+  #   updated_at        : datetime 
+  # =======================
+
   # Authlogic-driven
   acts_as_authentic
   
